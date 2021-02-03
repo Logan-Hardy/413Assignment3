5451f10ed214eb966556b76ecd72d273edc8a3d9
Logan-Hardy <70916374+Logan-Hardy@users.noreply.github.com>
2021-02-03 09:27:51 -0700

    Add project files.

---
+++ Models/TempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _413Assignment3.Models
+{
+    public static class TempStorage
+    {
+        private static List<MovieAddition> applications = new List<MovieAddition>();
+
+        public static IEnumerable<MovieAddition> Applications => applications;
+
+        public static void AddApplication(MovieAddition application)
+        {
+            applications.Add(application);
+        }
+    }
+}
